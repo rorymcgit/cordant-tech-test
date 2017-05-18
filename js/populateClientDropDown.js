e5cd78894043f026349bc71c43a7946852ce7fa8
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-05-18 20:52:27 +0100

    Dropdown list of clients visible on landing page

--- js/populateClientDropDown.js
+++ js/populateClientDropDown.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $.getJSON("/data/locations.json", function(data) {
-    console.log(data);
+    // console.log(data);
+    $.each(data.Clients, function(key, value) {
+      $("#dropDown").append($('<option></option>').val(value.name).html(value.name));
+    });
   });
 });
