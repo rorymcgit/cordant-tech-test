95d200f78bd147ed8f8d0d8bb8b44440f09ca160
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-05-22 21:52:10 +0100

    Implement array of distances and postcodes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+
 const API_KEY = process.env.API_KEY,
       candidates = require("./data/candidates.json").Candidates,
       getCandidatePostcodes = require("./js/getCandidatePostcodes"),
+      listAllCandidates = require("./js/listAllCandidates"),
       bodyParser = require("body-parser"),
       request = require("request"),
       fs = require("fs"),
@@ -19,15 +21,19 @@ app.get("/", function(req, res) {
   res.render("landingPage");
 });
 
+// var body = "{'destination_addresses' : [ 'Curzon St, Birmingham B4 7XG, UK' ],'origin_addresses' : ['Rocky Ln, Birmingham B42 1QZ, UK','Rocky Ln, Birmingham B42 1QX, UK'],'rows' : [{'elements' : [{'distance' : {'text' : '4.3 mi','value' : 6968},'duration' : {'text' : '11 mins','value' : 689},'status' : 'OK'}]},{'elements' : [{'distance' : {'text' : '4.6 mi','value' : 7368},'duration' : {'text' : '12 mins','value' : 728},'status' : 'OK'}]}],'status' : 'OK'}";
+// listAllCandidates(body);
+
 app.post("/candidates", function(req, res) {
-  // apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + getCandidatePostcodes(candidates) + "&destinations=" + req.body.client.split(" ").join("") + "&key=" + API_KEY;
+  // var apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + getCandidatePostcodes(candidates) + "&destinations=" + req.body.client.split(" ").join("") + "&key=" + API_KEY;
   var apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + "B421QZ|B421QX" + "&destinations=" + req.body.client.split(" ").join("") + "&key=" + API_KEY;
   // console.log(apiUrl);
   request(apiUrl, function(error, response, body) {
     console.log("error:", error);
     console.log("statusCode:", response && response.statusCode);
-    console.log("body:", body); // Print the HTML
-    res.render("candidates", { body: body });
+    console.log("body:", body);
+    listAllCandidates(body);
+    res.render("candidates", { body: body, listAllCandidates: listAllCandidates });
   });
 });
 
--- js/listAllCandidates.js
+++ js/listAllCandidates.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
-  $.getJSON("/data/candidates.json", function(data) {
-    $.each(data.Candidates, function(key, value) {
-      $("#candidates").append($("<li></li>").html(value.name + " " + value.postcode));
-    });
-  });
-});
+// $(document).ready(function(body) {
+//   $.getJSON("/data/candidates.json", function(data) {
+//     $.each(data.Candidates, function(key, value) {
+//       console.log(body);
+//       $("#candidates").append($("<li></li>").html(value.name + " " + value.postcode));
+//     });
+//   });
+// });
+
+var candidates = require("../data/candidates.json");
+
+function listAllCandidates(body) {
+  var apiResponse, postcodes, origin_addresses;
+  console.log(candidates.Candidates);
+  for (var i = 0; i < candidates.Candidates.length; i++) {
+    console.log(candidates.Candidates[i].name);
+    console.log(candidates.Candidates[i].postcode);
+    apiResponse = JSON.parse(body);
+    postcodes = apiResponse.rows;
+    origin_addresses = apiResponse.origin_addresses;
+  }
+  var distances = [];
+  for (var x = 0; x < postcodes.length && x < origin_addresses.length; x++) {
+    distances.push(postcodes[x].elements[0].distance, origin_addresses[x]);
+  }
+  console.log(distances);
+
+  // for (var y = 0; y < distances.length; y++) {
+  //   console.log(distances[y]);
+  // }
+}
+
+module.exports = listAllCandidates;
